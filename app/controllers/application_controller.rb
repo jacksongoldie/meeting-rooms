8df2e906e815eec1d0ced492a8e3e2fc75a1feef
root <root@LAPTOP-78CKJOIP.localdomain>
2022-03-05 12:40:04 -0600

    rescues setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_invalid(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def record_not_found
+    render json: { errors: 'Record not found' }, status: :not_found
+  end
+
 end
