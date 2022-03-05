ff219cefb24bb0919a47921b800e38d9a51be73f
root <root@LAPTOP-78CKJOIP.localdomain>
2022-03-05 12:27:02 -0600

    date validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one :review
+
+  validates :start_date, presence: true, before_or_equal_to: :end_date
+  validates :end_date, presence: true, after_or_equal_to: :start_date
 end
