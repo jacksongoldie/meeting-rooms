ca7c4d2ae544f4fec6fc9dc828b8827e944005d8
root <root@LAPTOP-78CKJOIP.localdomain>
2022-03-05 12:19:17 -0600

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :reservations
     has_many :rooms, through: :reservations
+
+    validates :name, presence: true, uniqueness: true, length: { greater_than: 6 }
+    validates :email, presence: true, uniqueness: true, email: true
 end
