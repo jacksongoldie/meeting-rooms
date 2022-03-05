739bc725a8860a05415f5364b9fc2ae94e5387eb
root <root@LAPTOP-78CKJOIP.localdomain>
2022-03-05 12:29:35 -0600

    review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :reservation
+
+  validates :description, length: { greater_than: 6, less_than: 500 }
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
