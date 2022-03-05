95a756bee6fbdb44399a29d8495d7df3793ac18d
root <root@LAPTOP-78CKJOIP.localdomain>
2022-03-05 12:34:30 -0600

    inital routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
-  resources :reviews
-  resources :users
-  resources :reservations
-  resources :rooms
+  #resources :reviews
+  #resources :users
+  resources :reservations, only: [:index, :create, :update, :destroy]
+  resources :rooms, only: [:show, :index]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
