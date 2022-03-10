b1e42af5cec82785ccfa60dd014f75662f3d8dce
jacksongoldie <goldiejackson25@yahoo.com>
2022-03-10 16:19:36 -0600

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    def index
+        render json: User.all
+    end
+
+    def show
+        user = User.find(session[:user_id])
+        render json: user, include: ['reservations', 'reservations.room']
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :name, :email, :account_id)
+    end
 end
