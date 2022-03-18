a75f71b48563ed6a4aa3a586beb482e0a1196267
jacksongoldie <goldiejackson25@yahoo.com>
2022-03-18 11:12:15 -0500

    reservation validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,8 +1,25 @@
 class Reservation < ApplicationRecord
-  belongs_to :room
   belongs_to :user
-  has_one :review
+  belongs_to :room
 
-  validates :start_date, presence: true, before_or_equal_to: :end_date
-  validates :end_date, presence: true, after_or_equal_to: :start_date
+  validates :start_date, :end_date, presence: true
+  validate :end_date_after_start_date
+  validate :start_date_after_today
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+ end
+
+ def start_date_after_today
+  if start_date < Date.today().to_s
+    errors.add(:start_date, "must be later than today\'s date")
+  end
+ end
+
 end
