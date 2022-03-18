8f143575d8f824ea366c281438dabf377f2b6447
jacksongoldie <goldiejackson25@yahoo.com>
2022-03-18 11:08:58 -0500

    set rescues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::API
 
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
+  
   private
 
   def record_invalid(invalid)
-    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    render json: { errors: invalid.record.errors.full_messages.map }, status: :unprocessable_entity
   end
 
   def record_not_found
