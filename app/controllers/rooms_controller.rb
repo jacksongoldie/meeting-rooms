a9245c5b07a5f07ec57519c27ab060f22ec64c5c
jacksongoldie <goldiejackson25@yahoo.com>
2022-03-18 11:10:15 -0500

    room routes

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,12 @@
 class RoomsController < ApplicationController
+
+    def index
+        rooms = Room.all
+        render json: rooms
+    end
+
+    def show
+        room = Room.find(params[:id])
+        render json: room
+    end
 end
