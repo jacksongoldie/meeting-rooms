61b6653f393f949bb25a3a7ac4a75e770bc868de
jacksongoldie <goldiejackson25@yahoo.com>
2022-03-18 11:16:17 -0500

    update user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+    has_secure_password
+
     has_many :reservations
     has_many :rooms, through: :reservations
+    belongs_to :account
 
-    validates :name, presence: true, uniqueness: true, length: { greater_than: 6 }
+    validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 50 }
+    validates :username, presence: true, uniqueness: true, length: { in: 3..22 }
     validates :email, presence: true, uniqueness: true, email: true
 end
