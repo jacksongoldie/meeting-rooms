6214d4141700f821623dc6f4b5bff023a655aa2a
jacksongoldie <goldiejackson25@yahoo.com>
2022-03-18 11:11:16 -0500

    login logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: ["Invalid Entry"] }, status: :unauthorized
+        end
+    end
+
+    def destroy
+        if session[:user_id]
+            session.destroy
+            head :no_content
+        end
+    end
+    
+end
