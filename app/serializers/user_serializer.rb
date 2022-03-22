22de7306303f3b4ce7e2a45e09c41b38dfd9dab1
jacksongoldie <goldiejackson25@yahoo.com>
2022-03-22 12:16:07 -0500

    adding reservations method to fetch in date order

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,8 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :name, :email
   belongs_to :account
   has_many :reservations
+
+  def reservations
+    object.reservations.order(start_date: :asc)
+  end
 end
