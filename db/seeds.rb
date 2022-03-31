a35506edeab50260ae1b5b60f2db61923a02ec37
jacksongoldie <goldiejackson25@yahoo.com>
2022-03-31 15:29:37 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,14 @@ User.destroy_all
 Room.destroy_all
 Reservation.destroy_all
 
+#Accounts
+candles = Account.create(business_name: 'Candle Co.', address: '201 E. Park Ave.')
+blocks = = Account.create(business_name: 'Building BlocksRUs', address: '202 E. Park Ave.')
+
 #Users
-goldie = User.create(name: 'Goldie Jackson', email: 'goldie@goldie.com')
-ilyas = User.create(name: 'Ilyas Rey', email: 'ilyas@goldie.com')
-louis = User.create(name: 'Louis Rose', email: 'louis@goldie.com')
+goldie = User.create(name: 'Goldie Jackson', email: 'goldie@goldie.com', password: 'grj1987', account_id: Account.first.id)
+ilyas = User.create(name: 'Ilyas Rey', email: 'ilyas@goldie.com', password: 'irj2011', account_id: Account.second.id)
+louis = User.create(name: 'Louis Rose', email: 'louis@goldie.com', account_id: Account.second.id)
 
 #Rooms
 blue = Room.create(name: 'Blue Room', number: 201, description: "A small conference room for up to 8 people.", amenities: "Adjustable Highback Chairs, A/V Equipment, Shared Catering Kitchen", image_url: 'https://media.istockphoto.com/photos/meeting-room-picture-id182160138?b=1&k=20&m=182160138&s=170667a&w=0&h=rH1LiL5l00z1QyQZG84loL3Mge7SQf5bJtGnieXAHqg=')
